290d751f34ba49b8baa2484b7bea3689ea46e44a
hoiekim <github@hoie.kim>
2024-08-02 22:57:27 -0700

    Initial Commit

---
+++ Keybaords/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Keybaords
+//
+//  Created by Hoie Kim on 8/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Keybaords/KeybaordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeybaordsApp.swift
+//  Keybaords
+//
+//  Created by Hoie Kim on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct KeybaordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
